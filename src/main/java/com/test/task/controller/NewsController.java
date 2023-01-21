01ee394413f170d5550681e4322174a82021de00
ereminsasha08 <SashaEremin08051998@gmail.com>
2023-01-21 12:17:46 +0500

    refactoring NewsController

--- src/main/java/com/test/task/controller/NewsController.java
+++ src/main/java/com/test/task/controller/NewsController.java
@@ -11,8 +11,12 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 @RestController()
 @RequestMapping("/api/news")
@@ -20,6 +24,13 @@ import java.util.Objects;
 public class NewsController {
     @Autowired
     private NewsService newsService;
+
+    private Map<String, BiFunction<String, Pageable, Page>> filters = new HashMap<>();
+
+    {
+        filters.put("topic", (value, pageable) -> newsService.findAllByTopic(value, pageable));
+        filters.put("source", (value, pageable) -> newsService.findAllBySource(value, pageable));
+    }
 
     @GetMapping()
     public ResponseEntity<Page<News>> getNews(@PageableDefault(sort = "title") Pageable pageable) {
@@ -38,13 +49,10 @@ public class NewsController {
     public Page<News> filterNews(@RequestParam(required = false, defaultValue = "") String filter,
                                  @RequestParam(required = false, defaultValue = "") String value,
                                  @PageableDefault(sort = "title") Pageable pageable) {
-        Page<News> filterNewsPage = null;
-        if ("topic".equalsIgnoreCase(filter)) {
-            filterNewsPage = newsService.findAllByTopic(value, pageable);
+        BiFunction<String, Pageable, Page> functionFilter = filters.get(filter.toLowerCase());
+        if (Objects.nonNull(functionFilter)) {
+            return functionFilter.apply(value, pageable);
         }
-        if ("source".equalsIgnoreCase(filter) && Objects.nonNull(value)) {
-            filterNewsPage = newsService.findAllBySource(value, pageable);
-        }
-        return filterNewsPage;
+        return null;
     }
 }
